07c5ee2a5d6a07ca109f8cc4a96ce8d1bb855703
twirrim <paulgraydon@hotmail.com>
2019-01-01 21:31:13 +0000

    Drop logging and unnecessary middle steps

--- python/sumoftheothers.py
+++ python/sumoftheothers.py
@@ -3,25 +3,17 @@
 """ This is an attempt to solve the Sum Of The Others problem on Kattis """
 
 import sys
-import logging
 
 
 def main():
     for entry in sys.stdin.readlines():
         data = entry.rstrip(" ").split(" ")
-        logging.info("Input: {}".format(data))
-        to_evaluate = '+'.join(data)
-        logging.info("Evaluate: {}".format(to_evaluate))
-        result = eval(to_evaluate)
-        logging.info("Result: {}".format(result))
+        result = eval('+'.join(data))
         for number in data:
             number = int(number)
             if result - number == number:
                 print(number)
                 break
 
-
-
 if __name__ == "__main__":
-    # logging.basicConfig(level=logging.INFO)
     main()
