01ed810a16f38549f665ad8d371bfc880819affe
twirrim <paulgraydon@hotmail.com>
2019-01-03 19:14:49 +0000

    initial version of sumkindofproblem and sumofothers for Rust

---
+++ rust/sumkindofproblem.rs
@@ -0,0 +1,21 @@
+use std::io::{self, BufRead};
+use std::io::Write;
+
+fn main() {
+    let stdin = io::stdin();
+    let _ignore = stdin.lock().lines().next().unwrap().unwrap();
+    let stdout = io::stdout();
+    let mut out = stdout.lock();
+    for line in stdin.lock().lines(){
+        let n : Vec<i32> = line.unwrap().split(' ')
+                               .map(|s| s.trim())
+                               .filter(|s| !s.is_empty())
+                               .map(|s| s.parse().unwrap())
+                               .collect();
+        let s1 = (n[1] * ( n[1] + 1))/ 2;
+        let s3 = s1 * 2;
+        let s2 = s3 - n[1];
+
+        writeln!(out, "{} {} {} {}", n[0], s1, s2, s3).unwrap();
+    };
+}
---
+++ rust/sumofothers.rs
@@ -0,0 +1,19 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines(){
+        let numbers : Vec<i32> = line.unwrap().split(' ')
+                                  .map(|s| s.trim())
+                                  .filter(|s| !s.is_empty())
+                                  .map(|s| s.parse().unwrap())
+                                  .collect();
+        let sum: i32 = numbers.iter().sum();
+        for number in &numbers{
+            if sum - *number == *number {
+                println!("{}", number.to_string());
+                break;
+            };
+        };
+    };
+}
