839adaef0ce15625b2aa69fb72965519fa9fa3de
twirrim <paulgraydon@hotmail.com>
2019-01-05 18:49:18 +0000

    First attempt at Another Candies, using python

---
+++ python/anothercandies.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+""" An attempt to solve the Another Candies problem on Kattis
+I should really do this in one loop. """
+
+import sys
+
+
+def confirm_its_possible(test_case):
+    children = int(test_case[0])
+    number_of_candies = 0
+    for i in range(1, children + 1):
+        number_of_candies += int(test_case[i])
+    if number_of_candies % children == 0:
+        print("YES")
+    else:
+        print("NO")
+
+
+def main():
+    # We don't care about the first line
+    sys.stdin.readline()
+    # We do care about the rest
+    data = sys.stdin.readlines()
+    test_cases = []
+    test_case = []
+    for line in data:
+        line = line.rstrip()
+        if not line:
+            if test_case:
+                test_cases.append(test_case)
+                test_case = []
+        else:
+            test_case.append(line)
+    test_cases.append(test_case)
+    for test_case in test_cases:
+        confirm_its_possible(test_case)
+
+
+if __name__ == "__main__":
+    main()
