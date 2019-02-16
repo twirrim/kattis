efc887ad8781124ee0c9893251711cd0ce8ed384
twirrim <paulgraydon@hotmail.com>
2019-02-16 17:10:23 +0000

    Added "A different problem" in Rust.

---
+++ src/bin/different.rs
@@ -0,0 +1,17 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    // set things up
+    let stdin = io::stdin();
+
+    for raw_line in stdin.lock().lines() {
+        let line = raw_line.unwrap();
+        let mut split = line.split(' ');
+        let first = split.next().unwrap()
+            .parse::<isize>().unwrap();
+        let second = split.next().unwrap()
+            .parse::<isize>().unwrap();
+        let diff = first - second;
+        println!("{}", diff.abs());
+    }
+}
