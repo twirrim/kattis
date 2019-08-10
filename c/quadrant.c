c593b98129c053fce6cdb4a19babf685d223eee8
Paul Graydon <paulgraydon@hotmail.com>
2019-08-10 18:35:51 +0100

    Add Quadrant Selection solution

---
+++ c/quadrant.c
@@ -0,0 +1,20 @@
+/* A solution to the Quadrant Selection problem on Kattis */
+#include <stdio.h>
+
+int main(void){
+    int x, y;
+    scanf("%d\n%d", &x, &y);
+    if (x < 0){
+        if (y > 0){
+            printf("2\n");
+        } else {
+            printf("3\n");
+        }
+    } else {
+        if (y > 0){
+            printf("1\n");
+        } else {
+            printf("4\n");
+        }
+    };
+}
