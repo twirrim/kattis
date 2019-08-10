e877a7ca577519ea81dbc803f54013fb10e82a0f
Paul Graydon <paulgraydon@hotmail.com>
2019-08-10 19:03:46 +0100

    C solution for Faktor

---
+++ c/faktor.c
@@ -0,0 +1,10 @@
+/* A solution to the Faktor problem on Kattis */
+#include <stdio.h>
+int main(void){
+    float articles, impact;
+    scanf("%f %f", &articles, &impact);
+    // Because we're rounding down, we can just decrement the target, multiply the first by it and add the 1 back in again
+    impact--;
+    float result = (articles * impact) + 1;
+    printf("%d", (int)result);
+}
